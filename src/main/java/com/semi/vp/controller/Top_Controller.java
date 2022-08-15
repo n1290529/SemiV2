324f5583a8c11229f465d2875a8fad7de6ec6ed9
n1290529 <bjks1290529@gn.iwasaki.ac.jp>
2022-08-15 17:13:47 +0900

    2022/08/15ヘッダー共有完成

--- src/main/java/com/semi/vp/controller/Top_Controller.java
+++ src/main/java/com/semi/vp/controller/Top_Controller.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Top_Controller {
 	/**
- * ユーザー情報一覧画面を表示
- * @param model Model
- * @return ユーザー情報一覧画面のHTML
- */
-@RequestMapping(value = "/top", method = RequestMethod.GET)	
-public String displayList(Model model) {
-  	return "HTML/002-01-01_Top";
-}
+	 * ユーザー情報一覧画面を表示
+	 * 
+	 * @param model Model
+	 * @return ユーザー情報一覧画面のHTML
+	 */
+	@RequestMapping(value = "/top", method = RequestMethod.GET)
+	public String displayList(Model model) {
+		return "HTML/002-01-01_Top";
+	}
 }
--- src/main/java/com/semi/vp/controller/User_profile_Controller.java
+++ src/main/java/com/semi/vp/controller/User_profile_Controller.java
@@ -1,19 +1,34 @@
 package com.semi.vp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.semi.vp.service.UserDetailsServiceImpl;
+
 @Controller
 public class User_profile_Controller {
+	@Autowired
+	UserDetailsServiceImpl userservise;
 	/**
- * ユーザー情報一覧画面を表示
- * @param model Model
- * @return ユーザー情報一覧画面のHTML
- */
-@RequestMapping(value = "/HTML/005-01_User_profile", method = RequestMethod.GET)	
-public String displayList(Model model) {
-  	return "HTML/005-01_User_profile";
-}
+	 * ユーザー情報一覧画面を表示
+	 * 
+	 * @param model Model
+	 * @return ユーザー情報一覧画面のHTML
+	 */
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	public String displayList(HttpServletRequest request, Model model) {
+		HttpSession session = request.getSession();
+		SecurityContext securityContext = (SecurityContext) session.getAttribute("SPRING_SECURITY_CONTEXT");
+		org.springframework.security.core.Authentication authentication = securityContext.getAuthentication();
+		
+//		model.addAttribute("user", userservise.oneReco(authentication.getName()));
+		return "HTML/005-01_User_profile";
+	}
 }
